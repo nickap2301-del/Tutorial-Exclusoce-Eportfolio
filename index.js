3f0b38dc30a8ac44c079666c2c9783ddd2a269be
nickap2301-del <nickap2301@gmail.com>
2026-08-07 18:23:46 -0500

    first commit

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-// template_q39byrs
-// service_coaksme 
-// bhUr8uq6lLF4dpzuV
+let isModalOpen = false;
+let contrastToggle = false;
 
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme")
+  }
+}
 function contact(event) {
   event.preventDefault();
-const loading = document.querySelector('.modal__overlay--loading')
-const success = document.querySelector('.modal__overlay--success') 
-loading.classList += " modal__overlay--visible";
-emailjs
-  .sendForm(
-  'service_coaksme',
-  'template_q39byrs',
-  event.target,
-  'bhUr8uq6lLF4dpzuV'
- ).then(() => {
-loading.classList.remove("modal__overlay--visible");
-success.classList += " modal__overlay--visible";
- }).catch(() => {
-  loading.classList.remove("modal__overlay--visible");
-alert(
-  "The email service is temporarily unavailable. Please contact me directly on nickap2301@gmail.com"
-);
-})
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_coaksme",
+      "template_q39byrs",
+      event.target,
+      "bhUr8uq6lLF4dpzuV",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on nickap2301@gmail.com",
+      );
+    });
 }
-let isModalOpen = false;
 function toggleModal() {
-  
   if (isModalOpen) {
-    isModalOpen = !isModalOpen;
-return document.body.classList.remove("modal--open");
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
   }
-  isModalOpen = !isModalOpen;
-document.body.classList += " modal--open";
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
